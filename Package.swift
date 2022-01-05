30df3695930ce937006b6369c91546b1f5dda8e1
Mykola Prodeus <mykola.prodeus@avenga.com>
2022-01-05 17:31:29 +0200

    1.8.2, update SPM

--- Package.swift
+++ Package.swift
@@ -2,4 +2,23 @@ import PackageDescription
 
 let package = Package(
     name: "RangeSeekSlider"
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "RangeSeekSlider",
+            targets: ["RangeSeekSlider"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "RangeSeekSlider",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "RangeSeekSliderTests",
+            dependencies: ["RangeSeekSlider"]
+        ),
+    ]
 )
--- RangeSeekSlider.podspec
+++ RangeSeekSlider.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name                  = 'RangeSeekSlider'
-  s.version               = '1.8.1'
+  s.version               = '1.8.2'
   s.summary               = 'RangeSeekSlider provides a customizable range slider like a UISlider.'
   s.description           = <<-DESC
                             RangeSeekSlider provides a customizable range slider like a UISlider.
